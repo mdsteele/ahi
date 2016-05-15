350bd9f818334fcf5f472e7ad81dcb8c6df11de0
Matthew D. Steele <mdsteele@alum.mit.edu>
2016-05-15 12:55:28 -0400

    Add Image struct

--- src/lib.rs
+++ src/lib.rs
@@ -71,8 +71,83 @@ impl Color {
     }
 }
 
+/// Represents a single ASCII Hex Image.
+#[derive(Clone)]
+pub struct Image {
+    width: u32,
+    height: u32,
+    pixels: Box<[Color]>,
+}
+
+impl Image {
+    /// Constructs a new image with all pixels transparent.
+    pub fn new(width: u32, height: u32) -> Image {
+        let num_pixels = (width * height) as usize;
+        return Image {
+            width: width,
+            height: height,
+            pixels: vec![Color::Transparent; num_pixels].into_boxed_slice(),
+        };
+    }
+
+    /// Returns the width of the image, in pixels.
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    /// Returns the height of the image, in pixels.
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    /// Returns a byte array containing RGBA-order data for the image pixels,
+    /// in row-major order.
+    pub fn rgba_data(&self) -> Vec<u8> {
+        let mut data = Vec::with_capacity(self.pixels.len() * 4);
+        for pixel in self.pixels.iter() {
+            let (r, g, b, a) = pixel.rgba();
+            data.push(r);
+            data.push(g);
+            data.push(b);
+            data.push(a);
+        }
+        data
+    }
+}
+
+impl std::ops::Index<(u32, u32)> for Image {
+    type Output = Color;
+    fn index(&self, index: (u32, u32)) -> &Color {
+        let (col, row) = index;
+        if col >= self.width || row >= self.height {
+            panic!("index out of range");
+        }
+        &self.pixels[(row * self.width + col) as usize]
+    }
+}
+
+impl std::ops::IndexMut<(u32, u32)> for Image {
+    fn index_mut(&mut self, index: (u32, u32)) -> &mut Color {
+        let (col, row) = index;
+        if col >= self.width || row >= self.height {
+            panic!("index out of range");
+        }
+        &mut self.pixels[(row * self.width + col) as usize]
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {}
+    fn image_rgba_data() {
+        let mut image = Image::new(2, 2);
+        image[(0, 0)] = Color::DarkRed;
+        image[(0, 1)] = Color::Green;
+        image[(1, 1)] = Color::Cyan;
+        assert_eq!(image.rgba_data(),
+                   vec![127, 0, 0, 255, 0, 0, 0, 0, 0, 255, 0, 255, 0, 255,
+                        255, 255]);
+    }
 }
